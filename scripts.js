9288eb900687d81acbff748c1ded9d92cf2ee8f0
Dev Joel <a12022@ae4outubro.pt>
2024-12-23 11:22:00 +0000

    I continued my self study

--- scripts.js
+++ scripts.js
@@ -4,3 +4,24 @@
 console.log(a);
 
 var a = 2;
+
+// Global scope
+var b = "Joel";
+let c = 16; 
+const d = "ID";
+
+
+// Local scope (Function)
+
+function MyFunc() {
+  let firstNumber = 3;
+  let secondNumber = 1;
+  
+// Local scope (Block)
+  {
+  console.log(firstNumber + secondNumber)
+  secondNumber = 3;
+  }
+}
+
+MyFunc()
