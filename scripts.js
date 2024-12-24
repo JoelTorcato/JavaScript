efde481b5143c3409cca764f278f94cffadcae9b
Dev Joel <a12022@ae4outubro.pt>
2024-12-24 12:34:13 +0000

    I studied the types of scopes

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ console.log(a);
 
 var a = 2;
 
-// Global scope
+/* Global scope
 var b = "Joel";
 let c = 16; 
 const d = "ID";
@@ -19,9 +19,45 @@ function MyFunc() {
   
 // Local scope (Block)
   {
-  console.log(firstNumber + secondNumber)
-  secondNumber = 3;
+    console.log(firstNumber + secondNumber)
+    secondNumber = 3;
   }
 }
+  
+MyFunc() */
 
-MyFunc()
+var x = 1; // Function scope
+let y = 2; // Block scope
+const z = 3 
+
+console.log(`Global: ${x}`);
+console.log(`Global: ${y}`);
+console.log(`Global: ${z}`);
+
+function myFunc() {
+  var x = 10;
+  const z = 5;
+
+  {
+    var x = 11; // Function scope
+    const z = 6; // Block scope
+
+    console.log(`Block: ${x}`);
+    console.log(`Block: ${y}`);
+    console.log(`Block: ${z}`);
+  }
+
+  console.log(`Function: ${x}`);
+  console.log(`Function: ${y}`);
+  console.log(`Function: ${z}`);
+}
+
+myFunc()
+
+// Average two grades
+
+let primeiraNota = prompt("Your first grade: ");
+let segundaNota = 3;
+
+console.log(primeiraNota + segundaNota / 2);
+
