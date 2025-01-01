c4d34bc748ba1d6d43818f6bbcf585fb39bb8645
Dev Joel <a12022@ae4outubro.pt>
2025-01-01 16:13:50 +0000

    I finished studying Map and Set, but I'm missing WeakMap and WeakSet

--- Node.js/Global keyword/Map/map.js
+++ Node.js/Global keyword/Map/map.js
@@ -8,3 +8,19 @@ function sample() {
   console.log('Hi')
 }
 console.log(myMap.set(sample, 'This is a function'));
+
+const calculator = {
+  add: (a, b) => a + b,
+  minus: (a, b) => a - b
+}
+
+const yourMap = new Map();
+
+myMap.set(calculator.add, 'This is a add function')
+myMap.set(calculator.minus, 'This is a minus function')
+
+function getDescription(_funName) {
+  return yourMap.get(_funName)
+}
+
+console.log(getDescription(calculator.minus))
