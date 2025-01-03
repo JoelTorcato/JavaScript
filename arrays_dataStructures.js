48c39de4d13817378c91aa6b7e20af90768fb8da
Dev Joel <a12022@ae4outubro.pt>
2025-01-03 08:34:04 +0000

    I continued my study on Structured Data

--- arrays_dataStructures.js
+++ arrays_dataStructures.js
@@ -1,4 +1,19 @@
 const names = ['Max', 'Manu', 'Julie', 'Max']
 
 // Index starts at zero
-console.log(names[1]);
+console.log(names[0]);
+console.log(names.length) // 3 = ['Max', 'Manu', 'Julie']
+
+for (const el of names) {
+  console.log(el);
+}
+names.push('Julie');
+console.log(names.length) // 4 = ['Max', 'Manu', 'Julie', 'Julie']
+
+// Function (Go through the entire matrix)
+const julieIndex = names.find(el => el === 'Julie');
+names.splice(2, 1);
+console.log(names);
+
+
+
